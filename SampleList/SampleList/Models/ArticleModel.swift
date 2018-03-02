7243b5bcf055825c498fc9e59a63693877435a16
Santos Ramon <santos.ramon.brito@gmail.com>
2018-03-01 21:49:11 -0300

    Added basic structure for the app

---
+++ SampleList/SampleList/Models/ArticleModel.swift
@@ -0,0 +1,9 @@
+//
+//  ArticleModel.swift
+//  SampleList
+//
+//  Created by Santos Ramon on 3/1/18.
+//  Copyright © 2018 Santos Ramon. All rights reserved.
+//
+
+import Foundation
---
+++ SampleList/SampleList/Views/NewsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  NewsTableViewCell.swift
+//  SampleList
+//
+//  Created by Santos Ramon on 3/1/18.
+//  Copyright © 2018 Santos Ramon. All rights reserved.
+//
+
+import UIKit
+
+class NewsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
